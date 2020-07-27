743fffbd3a9353673bd857390f78b502c696df24
Rohan Vahalia <r.vahalia@gmail.com>
2020-07-26 19:04:32 -0500

    Random updates

--- eloCalc.R
+++ eloCalc.R
@@ -1,6 +1,6 @@
 weeklyFrame = read.csv('./weekly_elo.csv', row.names = 1)
 
-print(row.names(weeklyFrame))
+
 
 elo_calc <- function(helo, hpts, aelo, apts, k = 60) {
   share_h = 10^(helo/400)
@@ -25,10 +25,12 @@ elo_gen <- function(weekFrame, prevWeek) {
   return(blanks)
 }
 
-week10mat = cbind(c(1, 7, 8, 3, 4, 9)
-                  , c(7, 2, 3, 5, 6, 4)
-                  , c(2, 12, 11, 6, 5, 10)
-                  , c(2, 7, 6, 3, 2, 5))
+print(row.names(weeklyFrame))
 
-weeklyFrame$week10 <- week10 <- elo_gen(week10mat, weeklyFrame$week9)
+week18mat = cbind(c(1, 2, 7, 8, 3, 5)
+                  , c(7, 4, 3, 7, 2, 7)
+                  , c(11, 4, 10, 9, 12, 6)
+                  , c(2, 5, 6, 2, 7, 2))
+
+weeklyFrame$week18 <- week18 <- elo_gen(week18mat, weeklyFrame$week17)
 write.csv(weeklyFrame, 'weekly_elo.csv')
